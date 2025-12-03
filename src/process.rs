334c4761a295e0d0d1b595ee639d59a4cf432fb6
futile <git@futile.eu>
2025-12-03 13:25:18 +0000

    process: clarify that `module` is case-sensitive

--- src/process.rs
+++ src/process.rs
@@ -67,6 +67,8 @@ impl Process {
     }
 
     /// get a `Module` of a `Process` by name
+    ///
+    /// note: the casing matters (don't search for `shell32.dll` if you want `SHELL32.dll`)
     pub fn module(&self, name: &str) -> Result<Module, Error> {
         let Some(snapshot) = snapshot::ModuleSnapshot::get_modules(self.id)?
             .into_iter()
